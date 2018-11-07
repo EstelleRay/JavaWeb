2f9429a083fd2ed205bafd61df6099f538be5cc8
EstelleRay <zhoukelo@126.com>
2018-11-07 11:58:22 +0800

    modify javaweb.sql

--- javaweb.sql
+++ javaweb.sql
@@ -1,5 +1,5 @@
 ﻿# Host: localhost  (Version: 5.5.20)
-# Date: 2018-10-24 11:20:05
+# Date: 2018-11-07 11:57:17
 # Generator: MySQL-Front 5.3  (Build 1.21)
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -36,6 +36,29 @@ CREATE TABLE `tb_user` (
 #
 
 INSERT INTO `tb_user` VALUES ('1','1','1','1','11','1','1'),('2','2','2','e0052341-92b8-4a48-abdc-211dfa4d3fd3_Koala.jpg','f','2','2');
+
+#
+# Source for table "tb_posts"
+#
+
+DROP TABLE IF EXISTS `tb_posts`;
+CREATE TABLE `tb_posts` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(255) NOT NULL DEFAULT '',
+  `title` varchar(255) NOT NULL DEFAULT '',
+  `content` text NOT NULL,
+  `posttime` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `pv` int(11) DEFAULT '0',
+  PRIMARY KEY (`Id`),
+  KEY `post_author_stuId` (`author`),
+  CONSTRAINT `post_author_stuId` FOREIGN KEY (`author`) REFERENCES `tb_user` (`stuId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+#
+# Data for table "tb_posts"
+#
+
+INSERT INTO `tb_posts` VALUES (1,'1','222','2222222222222','2018-11-07 11:52:45',0),(2,'1','333','33','2018-11-07 11:55:27',0);
 
 #
 # Source for table "users"
